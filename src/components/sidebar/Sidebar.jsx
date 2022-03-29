0d52737b4a638ea81b2fd533d7d2136145b75a83
kingorianderson <kingorianderson@gmail.com>
2022-03-29 17:19:32 +0300

    icons-material

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
+import HealthAndSafetyOutlinedIcon from "@mui/icons-material/HealthAndSafetyOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
 
 const Sidebar = () => {
   return (
@@ -10,16 +22,52 @@ const Sidebar = () => {
       <div className="center">
         <ul>
           <li>
+            <DashboardIcon />
             <span>Dashboard</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineIcon />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <StorefrontOutlinedIcon />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CreditCardIcon />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <AssessmentIcon />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsActiveOutlinedIcon />
+            <span>Notifications</span>
+          </li>
+          <li>
+            <HealthAndSafetyOutlinedIcon />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon />
+            <span>Settings</span>
+          </li>
+          <li>
+            <AccountCircleOutlinedIcon />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppOutlinedIcon />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
