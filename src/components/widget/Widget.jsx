9ce8feb9623256f9153221f5d1642dc036ce4bea
kingorianderson <kingorianderson@gmail.com>
2022-03-31 19:08:03 +0300

    commit

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,10 +1,70 @@
 import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
-const Widget = () => {
+const Widget = ({ type }) => {
+  let data;
+
+  //temporary
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: <PersonOutlinedIcon className="icon" />,
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: <ShoppingCartOutlinedIcon className="icon" />,
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: <MonetizationOnOutlinedIcon className="icon" />,
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: <AccountBalanceWalletOutlinedIcon className="icon" />,
+      };
+      break;
+    default:
+      break;
+  }
   return (
     <div className="widget">
-      <div className="left">left</div>
-      <div className="right">right</div>
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "KSH"}
+          {amount}
+        </span>
+        <span className="link">See all users</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          20%
+        </div>
+        <PersonOutlinedIcon className="icon" />
+      </div>
     </div>
   );
 };
