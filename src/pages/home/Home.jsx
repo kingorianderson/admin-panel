fdb476a8d3876100769335af74af70d15fc0054e
kingorianderson <kingorianderson@gmail.com>
2022-04-06 21:06:43 +0300

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,9 +14,9 @@ const Home = () => {
         <Navbar />
         <div className="widgets">
           <Widget type="user" />
+          <Widget type="product" />
           <Widget type="order" />
           <Widget type="earning" />
-          <Widget type="balance" />
         </div>
         <div className="charts">
           <Featured />
